b898b850dadd46c98fdc728e750d4e7c61e90b0a
wambui7 <shiko.sheila@gmail.com>
2018-08-02 21:33:54 -0500

    no changes

--- randomthreenumbers/Program.cs
+++ randomthreenumbers/Program.cs
@@ -6,7 +6,48 @@ namespace randomthreenumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Random random = new Random();
+            //int number = random.Next(0,30);
+            //break;
+
+            //Console.WriteLine(number);
+            //Console.ReadLine();
+
+            //char[] names = "names".ToCharArray();
+            //Random r = new Random();
+            //string randomstring = "";
+            //for (int i = 0; i < 30; i++)
+
+            Console.WriteLine("we are going to enter 30 names to choose a winner");
+            Console.Write("press enter on every name");
+            
+            
+           string[] names = new string [30];
+
+           Random r = new Random();
+
+
+
+            for (int namesmax = 0; namesmax < 30; namesmax++)
+            {
+                string Names = Console.ReadLine();
+
+                
+                {
+
+                }
+            }
+
+               
+               
+            {
+                
+           
+                
+            }
+            {
+              
+                          }
         }
     }
 }
